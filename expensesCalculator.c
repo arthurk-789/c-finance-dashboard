7454b5ddd289e4e8248d680dd7ecc1ee0e85ad0f
ethanVM91 <em966315@gmail.com>
2026-05-06 12:55:50 -0700

    Update expensesCalculator.c

--- expensesCalculator.c
+++ expensesCalculator.c
@@ -1,1 +1,6 @@
+#include <stdio.h>
+#include "expensesCalculator.h"
 
+void inputCategories(Catergory categories[], int *count){
+  printf("How many Categories do you need to fill out? (Car, Gas, Housing, etc) ");
+  scanf("%d", count);
