9a356a9dfffe28640965cd93f0d0d2a89000c177
AnthonyKesh <anthonykeshishian@icloud.com>
2026-05-09 18:55:13 -0700

    Fix variable declarations and input prompts in savingsTracker

--- savingsTracker.c
+++ savingsTracker.c
@@ -3,4 +3,18 @@
 
 void savingsTracker() {
   double goal;
-  double 
+  double currentSavings;
+  double monthlySavings;
+  double amountLeft;
+  double currentProgress
+  double monthsLeft;
+
+  printf("\n----- Savings Tracker -----\n");
+  printf("Enter the amount of money you want to save: $");
+  scanf("%lf", &goal);
+
+  printf("How much do you currently have saved: $");
+  scanf("%lfL, &currentSavings);
+
+  printf("How much are you willing to save every month: $");
+  scanf
