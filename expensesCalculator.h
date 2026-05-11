0998ebb92b438a165f62d2dc6a033bfee6e6a2a5
ethanVM91 <em966315@gmail.com>
2026-05-11 14:28:31 -0700

    Create expensesCalculator.h

---
+++ expensesCalculator.h
@@ -0,0 +1,18 @@
+#ifndef EXPENSESCALCULATOR_H
+#define EXPENSESCALCULATOR_H
+
+#define MAX_CATEGORIES 10
+
+typedef struct {
+  char name[50];
+  float percentage;
+  float amount;
+}
+
+Category;
+
+void inputCategories(Category categories[], int *count);
+void calculateExpenses(Category categories[], int count, float income);
+void displayExpenses(Category categories[], int count);
+
+#endif
